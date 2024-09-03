e32182d3d015dbeb61901e28415eda97e19382de
Nameandadresswithheld <143920032+Nameandadresswithheld@users.noreply.github.com>
2024-09-03 01:18:15 +0000

    sorry im late

--- Main.java
+++ Main.java
@@ -1,5 +1,42 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int fov(int i) {
+        return i;
     }
+    public static void foo() {
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to CS 112!");
+        foo();
+        fov(0);
+    }
+
+}
+
+class aaaa {
+    public int i;
+
+   public String toString()
+   {
+       return "0";
+   }
+
+   public void set(int j) {
+       i = j;
+   }
+
+   public int get() {
+       return i;
+   }
+
+   public aaaa() {
+       i = 0;
+   }
+
+   public aaaa(int j) {
+       i = j;
+   }
+
+   public boolean equals(int j) {
+       return i == j;  
+   }
 }
